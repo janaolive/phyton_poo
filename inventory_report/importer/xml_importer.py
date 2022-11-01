be7d358f5bdb2be6b292c0ce8acd47ab19248c9f
Janaina Oliveira <janainapp@gmail.com>
2022-11-01 18:40:10 -0300

    feat: add xml_importer.py

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,14 @@
+import xmltodict
+from inventory_report.importer.importer import Importer
+
+
+class XmlImporter(Importer):
+    @staticmethod
+    def import_data(path):
+        if ".xml" not in path:
+            raise ValueError("Arquivo inválido")
+        with open(path) as file:
+            line_content = file.read()
+            content = [
+                item for item in xmltodict.parse(line_content)["dataset"]["record"]
+            return content
